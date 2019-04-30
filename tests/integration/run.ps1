ed7b71f596acc688111ff9ac0481899bb5e3118d
Michal Meluch <meluch.m@gmail.com>
2019-04-30 09:53:01 +0200

    Added PostgreSQL start/stop to the run script

--- tests/integration/run.ps1
+++ tests/integration/run.ps1
@@ -1,11 +1,29 @@
 $output_dir = "output_files"
+$venv_path = "../../venv"
+
 $psql_path = "C:\Program Files\PostgreSQL\9.5\bin\psql.exe"
+$psql_service_name = "postgresql-x64-9.5"
+$psql_service = Get-Service -Name $psql_service_name
+
+if ($psql_service.Status -ne "Running") {
+    Write-host "Starting the PostgreSQL service"
+    Start-Service $psql_service_name
+    Start-Sleep -seconds 5
+    $psql_service.Refresh()
+
+    if ($psql_service.Status -eq 'Running') {
+        Write-Host "PostgreSQL database is running"
+    }
+    else {
+        Write-Host "Failed to start the PostgreSQL database"
+    }
+}
 
 # Run the DB setup script
 & $psql_path -U postgres -f test_db_setup.sql postgres
 
 # Activate the Python virtual env
-& "../../venv/Scripts/activate.ps1"
+& $venv_path"/Scripts/activate.ps1"
 
 # Start the application
 $proc = Start-Process python '../../run.py' -Passthru
@@ -26,3 +44,4 @@ robot --outputdir $output_dir example.robot
 # Do cleanup (the app itself has already been terminated via a REST call in RobotFramework)
 deactivate
 & $psql_path -U postgres -f test_db_teardown.sql postgres
+Stop-Service $psql_service_name
